a46c9a26c1c5a0c11d2b0f1fecc10cfb4b535df0
Aylin DeBruyne <adebruyne88@gmail.com>
2018-06-18 11:19:01 -0400

    add routes for /contacts and /contacts/:id

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) => {
+
+//Homepage! Show the user a welcome message
+app.get('/', (req, res) => {
     res.send('Haaaaayyyyyy');
 });
+
+//Contacts List page: show the user all contacts
+
+app.get('/contacts', (req, res) => {
+    res.send('You are on the listing page!!!')
+});
+
+//Contacts detail page: show the iser all info for one contact
+app.get('/contacts/:id', (req, res) => {
+    res.send(`You are viewing details for ${req.params.id}`);
+});
+
+
 
 
 app.listen(8888, () => {
